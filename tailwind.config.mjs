c1afd772122e32c68c77cf9695d39561fd1a6b23
EstDC <edc.modulo.fp@gmail.com>
2025-02-07 22:45:30 +0100

    Fix: corregido un error de importación de tailwind/typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -23,5 +24,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
-}
+	plugins: [typography],
+};
