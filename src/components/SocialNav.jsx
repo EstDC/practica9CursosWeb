08022bd202c163b67797b2885516766f98e677d0
EstDC <edc.modulo.fp@gmail.com>
2025-02-08 11:27:21 +0100

    Fix: arreglando susperposición elementos Hero

--- src/components/SocialNav.jsx
+++ src/components/SocialNav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/global.css';
 
 const SocialNav = () => (
-<div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex flex-col space-y-12 font-oswald z-40">
+<div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex flex-col space-y-12 font-oswald z-20">
   <a href="#" className="flex flex-col items-center space-y-11">
     <span className="text-white hover:text-red-500 text-sm uppercase tracking-widest -rotate-90 origin-center">Instagram</span>
     <img src="/img/instagram.png" className="w-9 h-9 -rotate-90" alt="Instagram"/>
